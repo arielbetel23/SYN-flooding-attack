6df0b0d6274e1e1bb95ef807b09c3c924fa82d4b
Ariel Betel <arielbetel7@gmail.com>
2026-08-26 18:25:04 +0300

    added the function the created the packet

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+import scapy.all as scapy
+
+
+def build_packet(ip, port):
+    ip_layer = scapy.IP(dst=ip)
+    tcp_layer = scapy.TCP(dport=port, flags="S", seq=1000)
+    packet = ip_layer/tcp_layer
+    return packet
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
